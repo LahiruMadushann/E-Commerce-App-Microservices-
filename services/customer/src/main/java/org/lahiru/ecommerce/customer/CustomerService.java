4b322e44329ad65d8c7cf1f17f8153bd9b521e15
LahiruMadushann <lahirumadushandl@gmail.com>
2025-02-08 17:14:40 +0530

    implement customer update api

--- services/customer/src/main/java/org/lahiru/ecommerce/customer/CustomerService.java
+++ services/customer/src/main/java/org/lahiru/ecommerce/customer/CustomerService.java
@@ -1,6 +1,9 @@
 package org.lahiru.ecommerce.customer;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
+import org.lahiru.ecommerce.exception.CustomerNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +17,29 @@ public class CustomerService {
         var customer = repository.save(mapper.toCustomer(request));
 
         return customer.getId();
+    }
+
+    public void updateCustomer(@Valid CustomerRequest request) {
+        var customer = repository.findById(request.id())
+                .orElseThrow(() -> new CustomerNotFoundException(
+                        String.format("Cannot update customer:: No customer found with the provided ID:: %s", request.id())
+                ));
+        mergerCustomer(customer, request);
+        repository.save(customer);
+    }
+
+    private void mergerCustomer(Customer customer, CustomerRequest request) {
+        if (StringUtils.isNotBlank(request.firstName())){
+            customer.setFirstName(request.firstName());
+        }
+        if (StringUtils.isNotBlank(request.lastName())){
+            customer.setLastName(request.lastName());
+        }
+        if (StringUtils.isNotBlank(request.email())){
+            customer.setEmail(request.email());
+        }
+        if (request.address() != null){
+            customer.setAddress(request.address());
+        }
     }
 }
---
+++ services/customer/src/main/java/org/lahiru/ecommerce/exception/CustomerNotFoundException.java
@@ -0,0 +1,10 @@
+package org.lahiru.ecommerce.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper=true)
+@Data
+public class CustomerNotFoundException extends RuntimeException {
+    private final String msg;
+}
