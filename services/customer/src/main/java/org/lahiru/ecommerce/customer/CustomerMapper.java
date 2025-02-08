c8aff22fed314a5cc5ab80655a67a30572144df7
LahiruMadushann <lahirumadushandl@gmail.com>
2025-02-08 16:15:39 +0530

    implement customer create api

---
+++ services/customer/src/main/java/org/lahiru/ecommerce/customer/CustomerMapper.java
@@ -0,0 +1,18 @@
+package org.lahiru.ecommerce.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerMapper {
+    public Customer toCustomer(CustomerRequest request) {
+        if (request == null) return null;
+
+        return Customer.builder()
+                .id(request.id())
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .address(request.address())
+                .build();
+    }
+}
