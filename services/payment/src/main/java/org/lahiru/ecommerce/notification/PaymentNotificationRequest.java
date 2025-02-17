5766128b0ee65d038193666bfeb74f296363b5ab
LahiruMadushann <lahirumadushandl@gmail.com>
2025-02-17 20:40:47 +0530

    implement producer

---
+++ services/payment/src/main/java/org/lahiru/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package org.lahiru.ecommerce.notification;
+
+import org.lahiru.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
--- services/payment/src/main/java/org/lahiru/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/org/lahiru/ecommerce/payment/PaymentService.java
@@ -2,6 +2,8 @@ package org.lahiru.ecommerce.payment;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.lahiru.ecommerce.notification.NotificationProducer;
+import org.lahiru.ecommerce.notification.PaymentNotificationRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +12,21 @@ public class PaymentService {
 
     private final PaymentRepository repository;
     private final PaymentMapper mapper;
+    private final NotificationProducer notificationProducer;
 
     public Integer createPayment(@Valid PaymentRequest request) {
         var payment = repository.save(mapper.toPayment(request));
-        return null;
+
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstname(),
+                        request.customer().lastname(),
+                        request.customer().email()
+                )
+        );
+        return payment.getId();
     }
 }
